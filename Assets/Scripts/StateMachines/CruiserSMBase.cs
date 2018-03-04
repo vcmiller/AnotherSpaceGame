45e9be98b12be6b84346037f634f57a81d09df01
Vincent Miller <vmiller0725@gmail.com>
2018-03-03 21:47:10 -0500

    adsf

---
+++ Assets/Scripts/StateMachines/CruiserSMBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SBR;
+
+public abstract class CruiserSMBase : StateMachine {
+    new protected CruiserChannels channels { get; private set; }
+
+    public override void Initialize() {
+        channels = GetComponent<Brain>().channels as CruiserChannels;
+
+        base.Initialize();
+    }
+
+    protected void MoveInDirection(Vector3 dir) {
+
+    }
+
+    protected float RotateTowards(Vector3 dir) {
+        dir = dir.normalized;
+
+        Vector3 c = Vector3.Cross(transform.forward, dir) * 10;
+        float d = Vector3.Dot(transform.forward, dir);
+
+        float sqr = c.sqrMagnitude;
+
+        if (sqr == 0) {
+            c = transform.right;
+        } else if (sqr > 1 || d < 0) {
+            c = c.normalized;
+        }
+
+        channels.rotation = c;
+
+        return Vector3.Dot(transform.forward, dir.normalized);
+    }
+}
